4a6e74969fc83cb906613cb160c228390ba69b15
chaningan <chaningan@gmail.com>
2023-01-04 14:29:01 -0800

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+function displayTemperature(response) {
+  console.log("temp:", response.data.main.temp);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let weatherDescription = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "49c0a79d5c55e2e846215d64443bcc56";
+let city = "San Diego";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
